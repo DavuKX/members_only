bf7984781faa68ff4c83e448c7a8f4059db8191d
DavuKX <duvandavid2001@gmail.com>
2022-07-30 00:55:47 +0000

    fix typo

--- post/urls.py
+++ post/urls.py
@@ -7,4 +7,5 @@ app_name = 'post'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('new_post/', views.new_post, name='new_post'),
 ]
--- post/views.py
+++ post/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
-
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 class PostListView(ListView):
     queryset = Post.objects.all()
@@ -23,4 +24,23 @@ def post_detail(request, year, month, day, post):
         request,
         'posts/post_detail.html',
         {'post': post}
+    )
+@login_required
+def new_post(request):
+    new_post = False
+    if request.method == 'POST':
+        post_form = PostForm(data=request.POST)
+        if post_form.is_valid():
+            new_post = post_form.save(commit=False)
+            new_post.author = request.user
+            new_post.status = 'Published'
+            new_post.save()
+    else:
+        post_form = PostForm()
+    
+    return render(
+        request,
+        'posts/new_post.html',
+        {'post_form': post_form,
+         'new_post': new_post}
     )
