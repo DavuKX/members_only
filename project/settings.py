08badf668a974b0b20de6ef484e73832e5bd9ac0
DavuKX <duvandavid2001@gmail.com>
2022-07-31 01:16:56 +0000

    add static root

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,7 @@ CRISPY_TEMPLATE_PACK = "tailwind"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
